b7131a12037bce9576df41ea7b71703a5db1e7c0
alimstest <43400703+alimstest@users.noreply.github.com>
2019-05-09 01:26:26 +0200

    Update lookup2.pp

--- manifests/lookup2.pp
+++ manifests/lookup2.pp
@@ -1,7 +1,5 @@
-notice("Apache is set to use ${lookup('apache_worker_factor', Integer)} workers")
-
-unless lookup('apparmor_enabled', Boolean) {
-  exec { 'apt-get -y remove apparmor': }
+lookup('users', Array[String]).each | String $username | {
+  user { $username:
+    ensure => present,
+  }
 }
-
-notice('dns_allow_query enabled: ', lookup('dns_allow_query', Boolean))
