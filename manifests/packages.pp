6ab1f728e1ecc8fad7b724ddd53b6a9454b98624
ALI DOGRU <ali_ms_test@outlook.com>
2019-05-09 23:14:37 +0200

    sada

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -6,7 +6,7 @@ file { '/tmp/AAAAA.txt':
   mode    => '0777',
 }
 
-$needs = [ 'htop', 'vlan', 'puppet-lint' , 'vim', 'openjdk-7-jdk', 'gnupg', 'rng-tools' ]
+$needs = [ 'htop', 'vlan', 'puppet-lint' , 'vim', 'gnupg', 'rng-tools' ]
 
 package { $needs:
   ensure => 'installed',
